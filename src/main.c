556f3506de88bb01f5f8bae3549ed231e04df16f
Yesua93 <66164337+Yesua93@users.noreply.github.com>
2020-06-03 20:28:23 -0400

    refactor main.c
    add multiple retries

--- src/main.c
+++ src/main.c
@@ -7,14 +7,18 @@
 int main() {
     char pathFile[MAXPATHNAME] = "./WORDS.txt";
     char *words[MAXNUMWORDS];
+    int retry;
     int num_elements = openFileWords(pathFile,words);
 
-
-    int selectedIndex = pickRandomWord(num_elements);
-    int lenghtSecret = strlen(words[selectedIndex]);
-    char secretWord[lenghtSecret];
-    fillSecret(&secretWord[0],lenghtSecret);
-    mainLoopGame(words[selectedIndex],&secretWord[0],lenghtSecret);
+    do{
+    	int selectedIndex = pickRandomWord(num_elements);
+    	int lenghtSecret = strlen(words[selectedIndex]);
+    	char secretWord[lenghtSecret];
+    	fillSecret(&secretWord[0],lenghtSecret);
+   	mainLoopGame(words[selectedIndex],&secretWord[0],lenghtSecret);
+	printf("\n\nQuieres volver a jugar?: 1.SI 2.NO \n");
+	scanf("%i",&retry);
+   } while(retry!=2);	
 
 
 
